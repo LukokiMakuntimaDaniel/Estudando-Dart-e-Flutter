d8d0ee2fbe04f9dc31736d24ab81bc2bfbdb801c
LukokiMakuntimaDaniel <lukokimakuntimadaniel@gmail.com>
2024-04-18 13:36:44 +0100

    criando button

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,21 +9,33 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  int contador=0;
+  int contador = 0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meu Aplicativo"),
+      ),
+      body: Center(
           child: GestureDetector(
-        child: Text('Contador: $contador'),
+        child: Text(
+          'Contador: $contador',
+          style: TextStyle(fontSize: 50),
+        ),
         onTap: () {
           setState(() {
             contador++;
           });
         },
-      ))
-      ,
-
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            contador++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
